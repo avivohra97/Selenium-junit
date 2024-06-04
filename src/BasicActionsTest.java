6049929edb54cdf256a8e238b14003170134d20e
avivohra97 <avivohra97@gmail.com>
2024-06-04 16:29:46 +0530

    Basic actions class, to login

---
+++ src/BasicActionsTest.java
@@ -0,0 +1,36 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class BasicActionsTest {
+    static WebDriver driver;
+    static String baseURL;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        driver  = new FirefoxDriver();
+        baseURL = "https://letskodeit.teachable.com/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void name() {
+        driver.get(baseURL);
+        driver.findElement(By.xpath("//a[@href='/sign_in']")).click();
+        driver.findElement(By.xpath("//input[@id='email']")).clear();
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("test@email.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("test");
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        driver.quit();
+
+    }
+}
